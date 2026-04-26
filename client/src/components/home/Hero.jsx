f11a3871c798b64ad2dc8ebc2d319b00bda521e0
selvagomu1992-afk <selvagomu1992@gmail.com>
2026-04-26 12:26:24 +0530

    fix logo import paths

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import logo from '../../../assets/logo.svg';
+import logo from '../../assets/logo.svg';
 
 const Hero = () => {
 
