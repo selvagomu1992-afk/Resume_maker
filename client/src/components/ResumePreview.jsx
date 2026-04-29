46c8f3be60fe70cf097ef8ff907b2af558f322dd
selvagomu1992-afk <selvagomu1992@gmail.com>
2026-04-29 20:59:12 +0530

    feat: add ResumePreview component and configure global styles with print media support

--- client/src/components/ResumePreview.jsx
+++ client/src/components/ResumePreview.jsx
@@ -22,42 +22,9 @@ const ResumePreview = ({data, template, accentColor, classes = ""}) => {
 
   return (
     <div className='w-full bg-gray-100'>
-      <div id="resume-preview" className={"border border-gray-200 print:shadow-none print:border-none " + classes}>
+      <div id="resume-preview" className={"border border-gray-200 " + classes}>
         {renderTemplate()}
       </div>
-
-      <style jsx>
-        {`
-        @page {
-          size: letter;
-          margin: 0;
-        }
-        @media print {
-          html, body {
-            width: 8.5in;
-            height: 11in;
-            overflow: hidden; 
-          }
-          body * {
-            visibility: hidden;
-          }
-          #resume-preview, #resume-preview * {
-            visibility: visible;
-          }
-          #resume-preview {
-            position: absolute;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: auto;
-            margin: 0;
-            padding: 0;
-            box-shadow: none !important;
-            border: none !important;
-          }
-        }
-        `}
-      </style>
     </div>
   )
 }
